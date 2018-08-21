465bd35915f29ab65de94dda51327501af23b5be
Jessica <a27422@NR-ISM176845.local>
2018-08-20 22:18:27 -0400

    adds an ajax function that dynamically adds the about me section to the page

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('a').on('click', function(e){
+    e.preventDefault();
+    var pageRef = $(this).attr('href');
+
+    callPage(pageRef);
+  })
+
+
+  function callPage(pageRefInput){
+    $.ajax({
+      url: pageRefInput,
+
+      type:"GET",
+
+      dataType: "text/html",
+
+      success: function(response){
+        $('.about')
+      }
+
+      error: function(error){
+        console.log('failed to retrieve page ', error);
+      }
+
+      
+    });
+
+
+
+})}
